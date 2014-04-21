10a45882621651bb31c3df2ce6bcc13d35772801
Zack Drach <hogbait@zdrach.com>
2014-04-21 05:00:46 +0000

    updates

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+# link config files
 ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/tmux.conf ~/.tmux.conf
-ln -sf $PWD/bashr_extra ~/.bashrc_extra
+ln -sf $PWD/bashrc_extra ~/.bashrc_extra
 
 
 # source .bashrc_extra from .bashrc
 touch ~/.bashrc
-cat ~/.bashrc | sed \
-    -e 's/source .bashrc_extra//' \
-    -e '$s/$/source .bashrc_extra/' > /tmp/bashrc
+cat ~/.bashrc | grep -v 'source .bashrc_extra' > /tmp/bashrc
+echo source .bashrc_extra >> /tmp/bashrc
 mv /tmp/bashrc ~/.bashrc
 
 
 # install scripts into the bin
 mkdir -p ~/bin
 ln -sf $PWD/trackpoint.sh ~/bin/
+
 
 # enable italic fonts in the terminal
 bash ./enable_italics.sh
