b09b7d721d88254ff891bb6aaa907dfcbda76385
Zack Drach <hogbait@gmail.com>
2015-05-09 13:29:22 -0700

    update unbuntu install

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -4,14 +4,6 @@
 bash $PWD/install_basic.sh
 
 
-# install scripts into the bin
-ln -sf $PWD/trackpoint.sh ~/bin/
-
-
-# enable italic fonts in the terminal
-bash ./enable_italics.sh
-
-
 # set git defaults
 git config --global user.email "hogbait@gmail.com"
 git config --global user.name "Zack Drach"
