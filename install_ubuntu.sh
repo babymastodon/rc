71a62c7459aa028a781a9e2eac59772af03ddb09
Zack Drach <hogbait@gmail.com>
2015-05-09 14:17:22 -0700

    blah

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -4,6 +4,10 @@
 bash $PWD/install_basic.sh
 
 
+ln -sf $PWD Xmodmap ~/.xmodmaprc
+ln -sf $PWD Xmodmap ~/.Xmodmap
+
+
 # set git defaults
 git config --global user.email "hogbait@gmail.com"
 git config --global user.name "Zack Drach"
