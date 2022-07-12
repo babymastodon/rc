876666877ea793c08c29788fee6cefd7161795ff
Zack Drach <hogbait@gmail.com>
2022-07-12 18:56:04 +0000

    fix_ubungu

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -4,10 +4,6 @@
 bash $PWD/install_basic.sh
 
 
-ln -sf $PWD Xmodmap ~/.xmodmaprc
-ln -sf $PWD Xmodmap ~/.Xmodmap
-
-
 # set git defaults
-git config --global user.email "hogbait@gmail.com"
+git config --global user.email "zackdrach@gmail.com"
 git config --global user.name "Zack Drach"
