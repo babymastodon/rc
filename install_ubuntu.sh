5ee479cf5f83a09d3176e91f9ff183da9d8d92bb
Zack Drach <zackdrach@gmail.com>
2025-10-04 22:27:45 -0700

    📚 docs — install_ubuntu.sh (+0/-9, 2025-10-04 22:27)

--- install_ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# common instructions
-bash $PWD/install_basic.sh
-
-
-# set git defaults
-git config --global user.email "zackdrach@gmail.com"
-git config --global user.name "Zack Drach"
