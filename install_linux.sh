87e10ab91ffcfb9ba79a1a00613fe12ac8cb167e
Zack Drach <zackdrach@gmail.com>
2025-10-04 22:39:46 -0700

    🐛 bugfix — install_linux.sh (+8/-1, 2025-10-04 22:39)

--- install_linux.sh
+++ install_linux.sh
@@ -3,4 +3,11 @@
 # common instructions
 bash $PWD/install_basic.sh
 
-ln -sf $PWD/bashrc_extra ~/.bashrc_extra
+ln -sf $PWD/bashrc_linux ~/.bashrc_linux
+
+# source .bashrc_linux from .bashrc
+touch ~/.bashrc
+cat ~/.bashrc | grep -v 'bashrc_linux' > /tmp/bashrc
+echo source ~/.bashrc_linux >> /tmp/bashrc
+mv /tmp/bashrc ~/.bashrc
+source ~/.bashrc
