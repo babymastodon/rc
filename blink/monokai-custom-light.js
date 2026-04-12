e7061cbe003b05bf6e7e88eafa4348a1e61dc3a5
Zachary Drach <zackdrach@gmail.com>
2026-04-12 12:25:20 -0700

    Add Blink Monokai theme variants

---
+++ blink/monokai-custom-light.js
@@ -0,0 +1,22 @@
+// Monokai Custom Light for Blink
+t.prefs_.set('color-palette-overrides', [
+  '#faf4f2',
+  '#e14775',
+  '#269d69',
+  '#cc7a0a',
+  '#e16032',
+  '#7058be',
+  '#1c8ca8',
+  '#998c9c',
+  '#d6d2d0',
+  '#8f1f43',
+  '#145c3d',
+  '#806005',
+  '#893518',
+  '#3f2e78',
+  '#0f5364',
+  '#29242a',
+]);
+t.prefs_.set('foreground-color', '#29242a');
+t.prefs_.set('background-color', '#faf4f2');
+t.prefs_.set('cursor-color', 'rgba(112,107,110,0.5)');
---
+++ blink/monokai-custom.js
@@ -0,0 +1,22 @@
+// Monokai Custom for Blink
+t.prefs_.set('color-palette-overrides', [
+  '#2d2a2e',
+  '#ff6188',
+  '#a9dc76',
+  '#ffd866',
+  '#fc9867',
+  '#ab9df2',
+  '#78dce8',
+  '#a1a19f',
+  '#4b464d',
+  '#ffa9cb',
+  '#d8ffb8',
+  '#fff5c3',
+  '#ffccae',
+  '#dfd0ff',
+  '#c0f9ff',
+  '#fcfcfa',
+]);
+t.prefs_.set('foreground-color', '#fcfcfa');
+t.prefs_.set('background-color', '#2d2a2e');
+t.prefs_.set('cursor-color', 'rgba(193,192,192,0.5)');
